909fe02f304fc6c583b8d677e5e28f4f6e33112c
harsh0989 <harshmangukiya1506@gmail.com>
2022-03-13 09:49:47 +0530

    Videos page created

--- client/src/pages/Videos.jsx
+++ client/src/pages/Videos.jsx
@@ -1,23 +1,35 @@
 import React from 'react'
-import {Box} from "@mui/material"
-import ReactPlayer from 'react-player'
-import Iframe from 'react-iframe'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper'
+import 'swiper/css/bundle';
+import { Card, CardContent } from '@mui/material';
+import ReactPlayer from 'react-player';
+import { margin } from '@mui/system';
 
-function Videos() {
+const Videos = () => {
+  const data = ['https://www.youtube.com/watch?v=aq3mnDRVIq0', 'https://www.youtube.com/watch?v=KVpxP3ZZtAc', 'https://www.youtube.com/watch?v=CH7DvnTNLuI', 'https://www.youtube.com/watch?v=ORAOkP1h3R0', 'https://www.youtube.com/watch?v=3iNpL_GmPh0', 'https://www.youtube.com/watch?v=Xg_l2oGNXm8', 'https://www.youtube.com/watch?v=iTrJVfvBAT0']
+
   return (
-    <Box>
-        <Box> 
-        <ReactPlayer url='https://www.youtube.com/watch?v=RpHCeooyqNo' controls="true" />
-        <Iframe url="http://streamlit.io"
-        width="450px"
-        height="450px"
-        id="myId"
-        className="myClassname"
-        display="initial"
-        position="relative"/>
-        </Box>
-
-    </Box>
+    <Swiper
+      modules={[Pagination]}
+      effect='cards'
+      spaceBetween={50}
+      slidesPerView={1}
+      pagination
+style={{marginBottom:'4.5%'}}
+    >
+      {
+        data.map((link) => {
+          return <SwiperSlide >
+            <Card sx={{ width: '70vw', height: '70vh' }}>
+              <CardContent>
+                <ReactPlayer url={link} controls={true} width='100%' height='68vh' />
+              </CardContent>
+            </Card>
+          </SwiperSlide>
+        })
+      }
+    </Swiper>
   )
 }
 
